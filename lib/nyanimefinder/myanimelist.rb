21ad34dac1580d8a9392b9a127c40e901cdd62ce
Nikita B. Zuev <nikitazu@gmail.com>
2013-07-03 16:53:25 +0400

    Unit tests made fast

--- lib/nyanimefinder/myanimelist.rb
+++ lib/nyanimefinder/myanimelist.rb
@@ -4,13 +4,29 @@ require 'net/http'
 
 module Nyanimefinder
   class MyAnimeList
+    
     def search_anime query
-      url  = "http://myanimelist.net/anime.php?q=#{query}"
-      html = Net::HTTP.get_response(URI.parse(url)).body
+      url      = "http://myanimelist.net/anime.php?q=#{query}"
+      response = Net::HTTP.get_response(URI.parse(url))
+      
+      if response.code == '302'
+        # if we got here, then we got single result
+        # and were redirected to a page with anime
+        redirect = URI.parse(response.header['location'])
+        response = Net::HTTP.get_response(redirect)
+        return parse_single_result response.body
+      else
+        return parse_multi_result response.body
+      end
+    end
+    
+    def parse_multi_result html
       doc  = Nokogiri::HTML(html)
       
+      # check for no results
+      # ====================
       not_found_div = doc.css('html>body>div#myanimelist>div#contentWrapper>div#content>div>div')[1]
-      if not_found_div.text == 'No titles that matched your query were found.' then
+      if not_found_div != nil and not_found_div.text == 'No titles that matched your query were found.' then
         return nil
       end
       
@@ -34,45 +50,19 @@ module Nyanimefinder
           #description: data[1].css('div.spaceit')[0].text.strip
         }
       end
-
+      
       return animes
     end
+    
+    def parse_single_result html
+      doc  = Nokogiri::HTML(html)
+      title = doc.css('html>body>div#myanimelist>div#contentWrapper>h1')[0].text.gsub! /^Ranked #\d+/i, ''
+      
+      anime = {
+        title: title
+      }
+      
+      return [anime]
+    end
   end
-
-  ROWHTML = <<-ROWHTML
-  <tr>
-        <td class="borderClass bgColor" width="50" valign="top">
-          <div class="picSurround">
-            <a href="http://myanimelist.net/anime/1587/Tenpou_Ibun_Ayakashi_Ayashi" class=
-            "hoverinfo_trigger" id="#sarea1587" rel="#sinfo1587"><img src=
-            "http://cdn.myanimelist.net/images/anime/11/4452t.jpg" border="0" /></a>
-          </div>
-        </td>
-
-        <td class="borderClass bgColor" valign="top">
-          <div id="sarea1587">
-            <div id="sinfo1587" rel="a1587" class="hoverinfo"></div>
-          </div><a href=
-          "http://myanimelist.net/anime/1587/Tenpou_Ibun_Ayakashi_Ayashi"><strong>Tenpou
-          Ibun Ayakashi Ayashi</strong></a> <a href=
-          "http://myanimelist.net/panel.php?go=add&amp;selected_series_id=1587&amp;hideLayout"
-          title="Quick add anime to my list" class="Lightbox_AddEdit button_add">add</a>
-
-          <div style="margin-top: 3px;" class="lightLink"></div>
-
-          <div class="spaceit">
-            In the year of Tenpo 14, Yoi, monsters from another world attack Edo. Those who
-            fight against them are members of Bansha Aratemesho. In public, Bansha
-            Aratemsho is known as an organization to study fo... <a href=
-            "http://myanimelist.net/anime/1587/Tenpou_Ibun_Ayakashi_Ayashi">read more.</a>
-          </div>
-        </td>
-
-        <td class="borderClass bgColor" align="center">TV</td>
-
-        <td class="borderClass bgColor" align="center">25</td>
-
-        <td class="borderClass bgColor" align="center">7.11</td>
-      </tr>
-  ROWHTML
 end
--- spec/myanimelist.rb
+++ spec/myanimelist.rb
@@ -1,31 +1,73 @@
 require 'nyanimefinder/myanimelist'
 
+HERE = File.dirname(File.expand_path(__FILE__))
+
 describe Nyanimefinder::MyAnimeList do
   it 'abracadabra should not be found' do
+    html = File.read File.join HERE, 'myanimelist_nothing.html'
     finder = Nyanimefinder::MyAnimeList.new
-    result = finder.search_anime 'abracadabra'
+    result = finder.parse_multi_result html
     result.should be_nil
   end
   
   it 'slayers should be found... obviously' do
+    html = File.read File.join HERE, 'myanimelist_multi.html'
     finder = Nyanimefinder::MyAnimeList.new
-    result = finder.search_anime 'slayers'
+    result = finder.parse_multi_result html
     result.should_not be_nil
-    result.map { |anime| anime[:title] }.should eql([
-      'Slayers', 
-      'Slayers Evolution-R', 
-      'Slayers Excellent', 
-      'Slayers Gorgeous',
-      'Slayers Great',
-      'Slayers Next',
-      'Slayers Premium',
-      'Slayers Return',
-      'Slayers Revolution',
-      'Slayers Special',
-      'Slayers Try',
-      'Slayers: The Motion Picture',
-      'Tenpou Ibun Ayakashi Ayashi', # why?
-      ])
+    result.should eql([
+      {:web_url=>"http://myanimelist.net/anime/534/Slayers", 
+        :title=>"Slayers", :type=>"TV", :series=>"26", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/6/19870t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/5233/Slayers_Evolution-R", 
+        :title=>"Slayers Evolution-R", :type=>"TV", :series=>"13", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/5/20938t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/1171/Slayers_Excellent", 
+        :title=>"Slayers Excellent", :type=>"OVA", :series=>"3", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/8/34993t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/867/Slayers_Gorgeous", 
+        :title=>"Slayers Gorgeous", :type=>"Movie", :series=>"1", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/5/34991t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/868/Slayers_Great", 
+        :title=>"Slayers Great", :type=>"Movie", :series=>"1", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/4/13097t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/535/Slayers_Next", 
+        :title=>"Slayers Next", :type=>"TV", :series=>"26", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/4/19918t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/866/Slayers_Premium", 
+        :title=>"Slayers Premium", :type=>"Movie", :series=>"1", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/2/20969t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/869/Slayers_Return", 
+        :title=>"Slayers Return", :type=>"Movie", :series=>"1", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/9/34997t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/4028/Slayers_Revolution", 
+        :title=>"Slayers Revolution", :type=>"TV", :series=>"13", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/7/20940t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/1170/Slayers_Special", 
+        :title=>"Slayers Special", :type=>"OVA", :series=>"3", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/8/20099t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/1172/Slayers_Try", 
+        :title=>"Slayers Try", :type=>"TV", :series=>"26", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/9/19872t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/536/Slayers:_The_Motion_Picture", 
+        :title=>"Slayers: The Motion Picture", :type=>"Movie", :series=>"1", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/10/2644t.jpg"},
+        
+      {:web_url=>"http://myanimelist.net/anime/1587/Tenpou_Ibun_Ayakashi_Ayashi", 
+        :title=>"Tenpou Ibun Ayakashi Ayashi", :type=>"TV", :series=>"25", 
+        :image_url=>"http://cdn.myanimelist.net/images/anime/11/4452t.jpg"}
+    ])
   end
   
   it 'slayers excellent should be found as a single one' do
@@ -33,8 +75,9 @@ describe Nyanimefinder::MyAnimeList do
     # when result is single item it opens anime main page
     # instead of search results list
     
+    html = File.read File.join HERE, 'myanimelist_single.html'
     finder = Nyanimefinder::MyAnimeList.new
-    result = finder.search_anime 'slayers+excellent'
+    result = finder.parse_single_result html
     result.should_not be_nil
     result.map { |anime| anime[:title] }.should eql(['Slayers Excellent'])
   end
