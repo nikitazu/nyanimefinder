c78fe01c9f38cf0dea140abd1c63a520f9f2e229
Nikita B. Zuev <nikitazu@gmail.com>
2013-07-03 20:51:27 +0400

    WorldArt.ru multi result search implemented

---
+++ lib/nyanimefinder/worldart.rb
@@ -0,0 +1,84 @@
+# encoding=utf-8
+require "nyanimefinder/version"
+require 'nokogiri'
+require 'net/http'
+
+module Nyanimefinder
+  class WorldArt
+    
+    def search_anime query
+      url      = "http://myanimelist.net/anime.php?q=#{query}"
+      response = Net::HTTP.get_response(URI.parse(url))
+      
+      if response.code == '302'
+        # if we got here, then we got single result
+        # and were redirected to a page with anime
+        redirect = URI.parse(response.header['location'])
+        response = Net::HTTP.get_response(redirect)
+        return parse_single_result response.body
+      else
+        return parse_multi_result response.body
+      end
+    end
+    
+    def parse_multi_result html
+      doc  = Nokogiri::HTML(html)
+      
+      result = doc.css('html>body table')[6]
+      animes = []
+      rows = result.css('tr')
+      rows = rows[5, rows.count - 6]
+      
+      if rows == nil # nothing was found
+        return nil
+      end
+      
+      rows.each do |row|
+        link = row.css('td a')
+        url = link.attr('href')
+        web_url = "http://www.world-art.ru/#{url}"
+        
+        match = /\((\d{4}), (\p{Word}+), (\p{Any}+)\)/.match(link.text)
+        title = link.text
+        year = match[1]
+        country = match[2]
+        type_and_series = match[3]
+        
+        type = ''
+        series = ''
+        if type_and_series == 'полнометражный фильм' || 
+           type_and_series == 'короткометражный фильм' then
+          type = 'Movie'
+          series = '1'
+        else
+          tsmatch = /(\p{Word}+), (\d+)/.match(type_and_series)
+          if tsmatch != nil then
+            type = tsmatch[1]
+            series = tsmatch[2]
+            
+            if type == 'ТВ' then type = 'TV' end
+          end
+        end
+        
+        animes << {
+          :web_url    => web_url,
+          :title      => title,
+          :type       => type,
+          :series     => series,
+          :year       => year,
+          :country    => country
+        }
+        
+      end
+      
+      return animes
+    end
+    
+    def parse_single_result html
+      doc  = Nokogiri::HTML(html)
+      
+      return nil
+    end
+
+  end
+end
---
+++ spec/worldart.rb
@@ -0,0 +1,73 @@
+# encoding=utf-8
+require 'nyanimefinder/worldart'
+
+HERE = File.dirname(File.expand_path(__FILE__))
+
+describe Nyanimefinder::WorldArt do
+  it 'not results should be parsed as nil' do
+    html = File.read File.join HERE, 'worldart_nothing.html'
+    finder = Nyanimefinder::WorldArt.new
+    result = finder.parse_multi_result html
+    result.should be_nil
+  end
+  
+  it 'many results should be parsed as list of maps' do
+    html = File.read File.join HERE, 'worldart_multi.html'
+    finder = Nyanimefinder::WorldArt.new
+    result = finder.parse_multi_result html
+    result.should_not be_nil
+    result.should eql([      
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=7101", :title=>"Рубаки: Эволюция-Эр (2009, Япония, ТВ, 13 эп.)", :type=>"TV", :series=>"13", :year=>"2009", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=6896", :title=>"Рубаки: Революция (2008, Япония, ТВ, 13 эп.)", :type=>"TV", :series=>"13", :year=>"2008", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=5953", :title=>"Странная история эпохи Тэмпо: Аякасиаяси (2006, Япония, ТВ, 25 эп. + 5 DVD-спэшлов)", :type=>"TV", :series=>"25", :year=>"2006", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=3699", :title=>"Slayers Premium (2001, Япония, короткометражный фильм)", :type=>"Movie", :series=>"1", :year=>"2001", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=203", :title=>"Превосходные Рубаки (1998, Япония, OVA, 3 эп.)", :type=>"OVA", :series=>"3", :year=>"1998", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=204", :title=>"Slayers Gorgeous (1998, Япония, полнометражный фильм)", :type=>"Movie", :series=>"1", :year=>"1998", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=3270", :title=>"Затерянная Вселенная (1998, Япония, ТВ, 26 эп.)", :type=>"TV", :series=>"26", :year=>"1998", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=202", :title=>"Великие Рубаки на большом экране (1997, Япония, полнометражный фильм)", :type=>"Movie", :series=>"1", :year=>"1997", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=732", :title=>"Рубаки Try [ТВ] (1997, Япония, ТВ, 26 эп.)", :type=>"TV", :series=>"26", :year=>"1997", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=200", :title=>"Возвращение Рубак на большой экран (1996, Япония, полнометражный фильм)", :type=>"Movie", :series=>"1", :year=>"1996", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=201", :title=>"Особые Рубаки (1996, Япония, OVA, 3 эп.)", :type=>"OVA", :series=>"3", :year=>"1996", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=731", :title=>"Рубаки Некст [ТВ] (1996, Япония, ТВ, 26 эп.)", :type=>"TV", :series=>"26", :year=>"1996", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=199", :title=>"Рубаки на большом экране (1995, Япония, полнометражный фильм)", :type=>"Movie", :series=>"1", :year=>"1995", :country=>"Япония"},
+      {:web_url=>"http://www.world-art.ru/animation/animation.php?id=730", :title=>"Рубаки [ТВ] (1995, Япония, ТВ, 26 эп.)", :type=>"TV", :series=>"26", :year=>"1995", :country=>"Япония"},
+    ])
+  end
+  
+  it 'single result should be parsed as list of maps too' do
+    # myanimelist.net behaves smartassy for its users
+    # when result is single item it opens anime main page
+    # instead of search results list
+    
+    html = File.read File.join HERE, 'worldart_single.html'
+    finder = Nyanimefinder::WorldArt.new
+    result = finder.parse_single_result html
+    result.should_not be_nil
+    result.should eql([{
+      :web_url    =>  "http://myanimelist.net/anime/1171/Slayers_Excellent", 
+      :title      =>  "Slayers Excellent", 
+      :type       =>  "OVA", 
+      :series     =>  "3", 
+      :image_url  =>  "http://myanimelist.net/anime/1171/Slayers_Excellent/pic&pid=34993",
+      :airing     =>  "finished"
+    }])
+  end
+  
+  it 'single result should be parsed as list of maps with additional info' do
+    html = File.read File.join HERE, 'worldart_single2.html'
+    finder = Nyanimefinder::WorldArt.new
+    result = finder.parse_single_result html
+    result.should_not be_nil
+    result.should eql([{
+      :web_url    =>  "http://myanimelist.net/anime/16498/Shingeki_no_Kyojin", 
+      :title      =>  "Shingeki no Kyojin",
+      :type       =>  "TV", 
+      :series     =>  "25", 
+      :image_url  =>  "http://myanimelist.net/anime/16498/Shingeki_no_Kyojin/pic&pid=47347",
+      :airing     =>  "currently",
+      :other_titles => { 
+        "English" => "Attack on Titan"
+      }
+    }])
+  end
+end
